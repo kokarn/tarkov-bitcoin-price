f60039fe450eadced82f8e233d0484ba46709c9e
Oskar Risberg <oskar.risberg@stendahls.se>
2022-01-06 13:44:34 +0100

    use the tarkov tools api

--- build.js
+++ build.js
@@ -12,7 +12,7 @@ const formatPrice = (price) => {
     return currencyFormat.substr(0, currencyFormat.length - 2);
 };
 
-https.get('https://api.coindesk.com/v1/bpi/currentprice/rub.json', (res) => {
+https.get('https://tarkov-tools.com/graphql?query={%20item(id:%20%2259faff1d86f7746c51718c9c%22)%20{%20sellFor%20{%20source%20price%20}%20}%20}', (res) => {
     console.log('statusCode:', res.statusCode);
     console.log('headers:', res.headers);
 
@@ -21,7 +21,8 @@ https.get('https://api.coindesk.com/v1/bpi/currentprice/rub.json', (res) => {
 
         console.log(parsedData);
 
-        const output = source.replace(/BTC_PRICE/g, formatPrice(Math.floor(parsedData.bpi.RUB.rate_float * 0.085)));
+        const therapistPrice = parsedData.data.item.sellFor.find(price => price.source === 'therapist');
+        const output = source.replace(/BTC_PRICE/g, formatPrice(therapistPrice.price));
 
         fs.writeFileSync('./site/index.html', output);
     });
